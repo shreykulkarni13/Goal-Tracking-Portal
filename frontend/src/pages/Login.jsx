3767752e5641349fe81ef85caa18dc6aba917106
shreykulkarni13 <shreykulkarni13@gmail.com>
2026-06-12 01:48:05 +0530

    login working

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,64 @@
-import React from "react";
+import { useState } from "react";
+import { supabase } from "../supabase/supabaseClient";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    const { data, error } =
+      await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+    console.log("LOGIN DATA:", data);
+    console.log("LOGIN ERROR:", error);
+
+    if (error) {
+      alert(error.message);
+      return;
+    }
+
+    alert("Login Successful 🎉");
+
+    navigate("/dashboard");
+  };
+
   return (
     <div>
       <h1>Login</h1>
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) =>
+          setEmail(e.target.value)
+        }
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) =>
+          setPassword(e.target.value)
+        }
+      />
+
+      <br />
+      <br />
+
+      <button onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 }
