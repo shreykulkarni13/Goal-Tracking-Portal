fb26d2b05ffc3e422c81cb11caef9d96fc98e6e1
shreykulkarni13 <shreykulkarni13@gmail.com>
2026-06-15 04:04:31 +0530

    Admin dashboard also done ❤

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,13 +1,67 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../supabase/supabaseClient";
 
 function AdminDashboard() {
   const navigate = useNavigate();
+  
+  const [employeeCount, setEmployeeCount] = useState(0);
+  const [managerCount, setManagerCount] = useState(0);
+
+  const [totalGoals, setTotalGoals] = useState(0);
+  const [pendingGoals, setPendingGoals] = useState(0);
+  const [approvedGoals, setApprovedGoals] = useState(0);
+  const [rejectedGoals, setRejectedGoals] = useState(0);
 
   useEffect(() => {
     checkRole();
+    fetchStats();
   }, []);
+
+  async function fetchStats() 
+  {
+      const { data: profiles } = await supabase
+        .from("profiles")
+        .select("role");
+    
+      const { data: goals } = await supabase
+        .from("goals")
+        .select("status");
+    
+      setEmployeeCount(
+        profiles.filter(
+          (user) => user.role === "employee"
+        ).length
+      );
+    
+      setManagerCount(
+        profiles.filter(
+          (user) => user.role === "manager"
+        ).length
+      );
+    
+      setTotalGoals(goals.length);
+    
+      setPendingGoals(
+        goals.filter(
+          (goal) => goal.status === "pending"
+        ).length
+      );
+    
+      setApprovedGoals(
+        goals.filter(
+          (goal) => goal.status === "approved"
+        ).length
+      );
+    
+      setRejectedGoals(
+        goals.filter(
+          (goal) => goal.status === "rejected"
+        ).length
+      );
+
+      
+  }
 
   async function checkRole() {
     const {
@@ -39,6 +93,23 @@ function AdminDashboard() {
     <div>
       
       <h1>Admin Dashboard</h1>
+
+
+       <h2>Users</h2>
+         <p>Employees: {employeeCount}</p>
+         
+         <p>Managers: {managerCount}</p>
+         
+         <h2>Goals</h2>
+         
+         <p>Total Goals: {totalGoals}</p>
+         
+         <p>Pending Goals: {pendingGoals}</p>
+         
+         <p>Approved Goals: {approvedGoals}</p>
+         
+         <p>Rejected Goals: {rejectedGoals}</p>
+         
 
       <button onClick={async () => {
         await supabase.auth.signOut();
